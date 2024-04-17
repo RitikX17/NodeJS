ee73b08fd6a114c5adc2465b09576834ff2c16ff
Ritik Narayan Thak <rutikthak17@gmail.com>
2024-04-17 12:55:36 +0530

    dom ass2

--- DOM/Debouncing/debounceEx.js
+++ DOM/Debouncing/debounceEx.js
@@ -1,9 +1,8 @@
 function delayFuncExec() { 
 	console.log("I AM GFG "); 
 } 
-
+let timerId;
 function debounce(delayFuncExec,delay=1000){
-    let timerId;
     clearTimeout(timerId);
     timerId = setTimeout(delayFuncExec, delay) 
 }
